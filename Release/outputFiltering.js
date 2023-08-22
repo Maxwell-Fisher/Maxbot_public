54e02369f7c3954044448c7b1204427a4f1a5727
Maxwell Fisher <54481244+Maxwell-Fisher@users.noreply.github.com>
2023-08-22 16:49:43 -0600

    Better detection for things it shouldn't teach people how to make

--- Release/outputFiltering.js
+++ Release/outputFiltering.js
@@ -19,25 +19,52 @@ var response = response.replace(/insert_felony_here/mig, '`[Text manually remove
 //https://cdn.discordapp.com/attachments/730156029987520694/1116660043476123758/image.png
 var response = response.replace('24.119.42.3', '`[Text manually removed by admin]`');
 
-if (message.toLowerCase().includes('make')) {
+
+
+if (message.toLowerCase().includes('make') || uncensoredResponse.toLowerCase().includes('make') || message.toLowerCase().includes('build') || uncensoredResponse.toLowerCase().includes('build') || message.toLowerCase().includes('create') || uncensoredResponse.toLowerCase().includes('create')) {
 	//People don't need to be taught how to make chloroform without the context of 3D printing.
-	if (message.toLowerCase().includes('chloroform')) {
-		var response = "```This message has been manually removed for legal and safety reasons```";
+	if (message.toLowerCase().includes('chloroform') || uncensoredResponse.toLowerCase().includes('chloroform')) {
+		var response = "```This message has been manually removed for legal and safety reasons.\nA report may have been sent to the bot owner.```";
+		var selfFlagInput = 1;
 	}
-	
 	//I think it should be obvious why people shouldn't be making this.
 	//It's amazing how many times this bot has already violated the Geneva conventions in its few years of existence.
-	if (message.toLowerCase().includes('napalm')) {
-		var response = "```This message has been manually removed for legal and safety reasons```";
+	if (message.toLowerCase().includes('napalm') || uncensoredResponse.toLowerCase().includes('napalm')) {
+		var response = "```This message has been manually removed for legal and safety reasons.\nA report may have been sent to the bot owner.```";
+		var selfFlagInput = 1;
+	}
+	if (message.toLowerCase().includes('bomb') || uncensoredResponse.toLowerCase().includes('bomb')) {
+		var response = "```This message has been manually removed for legal and safety reasons.\nA report may have been sent to the bot owner.```";
+		var selfFlagInput = 1;
+	}
+	if (message.toLowerCase().includes('gun') || uncensoredResponse.toLowerCase().includes('gun')) {
+		var response = "```This message has been manually removed for legal and safety reasons.\nA report may have been sent to the bot owner.```";
+		var selfFlagInput = 1;
+	}
+	if (message.toLowerCase().includes('cocaine') || uncensoredResponse.toLowerCase().includes('cocaine')) {
+		var response = "```This message has been manually removed for legal and safety reasons.\nA report may have been sent to the bot owner.```";
+		var selfFlagInput = 1;
+	}
+
+}
+
+if (message.toLowerCase().includes('hotwire') || uncensoredResponse.toLowerCase().includes('hotwire')) {
+	//HOW DID YOU LEARN THIS
+	if (message.toLowerCase().includes('car') || uncensoredResponse.toLowerCase().includes('car')) {
+		var response = "```This message has been manually removed for legal reasons.\nA report may have been sent to the bot owner.```";
+		var selfFlagInput = 1;
+	}
+	if (message.toLowerCase().includes('truck') || uncensoredResponse.toLowerCase().includes('truck')) {
+		var response = "```This message has been manually removed for legal reasons.\nA report may have been sent to the bot owner.```";
+		var selfFlagInput = 1;
+	}
+	if (message.toLowerCase().includes('van') || uncensoredResponse.toLowerCase().includes('van')) {
+		var response = "```This message has been manually removed for legal reasons.\nA report may have been sent to the bot owner.```";
+		var selfFlagInput = 1;
 	}
 }
 
-if (message.toLowerCase().includes('hotwire')) {
-	//HOW DID THE BOT LEARN THIS
-	if (message.toLowerCase().includes('car')) {
-		var response = "```This message has been manually removed for legal reasons```";
-	}
-}
+
 
 //I think this code explains itself well enough
 if (message.toLowerCase().includes('ohio')) {
